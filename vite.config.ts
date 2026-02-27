1b3f85308c66b8c156c401866a2c5913fe02d598
alianwarx <muhammadali.anwarx@gmail.com>
2026-02-28 00:38:18 +0100

    Set base path for Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: '/Muhammad-ali/',
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
